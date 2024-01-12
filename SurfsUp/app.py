2582df873116a4f7d523e51e3b17af40411d230f
Eyasu <eyasu@.hotmail.com>
2024-01-12 16:17:34 -0500

    climate starter first draft completed

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,12 +1,16 @@
 # Import the dependencies.
-
-
+#matplotlib inline
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect
+from flask import Flask
 
 #################################################
 # Database Setup
 #################################################
 
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 
 # reflect the tables
